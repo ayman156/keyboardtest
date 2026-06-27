a5ed2b9dc5c5e7974216cc6bb082ea6532a63434
ayman156 <aymandev01@gmail.com>
2026-06-27 21:14:19 +0300

    Updated project file: app/src/main/java/ayman/djangogenerator/MysqlActivity.java via SketchGit

---
+++ app/src/main/java/ayman/djangogenerator/MysqlActivity.java
@@ -0,0 +1,144 @@
+package ayman.djangogenerator;
+
+import  android.database.Cursor;
+import  android.database.SQLException;
+import  android.database.sqlite.SQLiteDatabase;
+import  android.database.sqlite.SQLiteException;
+import  java.io.File;
+import  java.util.ArrayList;
+import  java.util.HashMap;
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import aym.lib.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+
+public class MysqlActivity extends AppCompatActivity {
+	
+	private EditText edittext1;
+	private LinearLayout linear1;
+	private ScrollView vscroll1;
+	private Button button1;
+	private LinearLayout linear2;
+	private EditText edittext2;
+	
+	private EasyDB db;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.mysql);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		edittext1 = findViewById(R.id.edittext1);
+		linear1 = findViewById(R.id.linear1);
+		vscroll1 = findViewById(R.id.vscroll1);
+		button1 = findViewById(R.id.button1);
+		linear2 = findViewById(R.id.linear2);
+		edittext2 = findViewById(R.id.edittext2);
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				edittext2.setText(db.executeRawSQLToJSON(edittext1.getText().toString()));
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		db = new EasyDB("data/data/"+ "ayman.djangogenerator/" +"database", "django_builder");
+		
+		
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (db != null) {
+			db.close();
+		}
+		
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
