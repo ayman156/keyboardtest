95f5b77b27660d206a1f91847d35a42532aa4b57
ayman156 <aymandev01@gmail.com>
2026-06-27 21:16:15 +0300

    Updated project file: app/src/main/java/ayman/djangogenerator/ModelObj.java via SketchGit

---
+++ app/src/main/java/ayman/djangogenerator/ModelObj.java
@@ -0,0 +1,11 @@
+package ayman.djangogenerator;
+
+public class ModelObj {
+    public long id;
+    public String name;
+
+    public ModelObj(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
